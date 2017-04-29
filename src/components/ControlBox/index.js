b0dad08b2e24e0a99e88a7a98684ff98bfaf11b4
Xaiana <melisskann@gmail.com>
2017-04-29 14:03:00 -0700

    installed react-select 1.0.0-rc modified controlbox

--- src/components/ControlBox/index.js
+++ src/components/ControlBox/index.js
@@ -4,15 +4,31 @@ import { GeoJSON } from 'react-leaflet';
 import { connect } from 'react-redux';
 import { getFmasThunk, getFmasData, renderFmaPanelId, getFmaPanelId } from '../../state';
 import { MapPanel } from '../index';
+import Select from 'react-select';
 
 const portland = [45.52, -122.67];
 
-class ControlBox extends Component {
+class ControlBox extends React.Component {
+  constructor() {
+    this.state = {
+      options: [
+        {value: 'Nearby Projects', text: 'Nearby Projects'},
+        {value: 'Project Conflicts', text: 'Project Conflicts'},
+        {value: 'Projects by Source', text: 'Projects by Source'}
+      ]
+    }
+  }
+
 
   render() {
     return (
-      <div>
-        Control Buttons Here
+      <div class='dropdown'>
+        <button class='dropdown' type='dropdown' data-toggle='dropdown'>Search</button>
+        <ul class='drowpdown-menu'>
+          <li>Conflicts</li>
+          <li>Projects</li>
+          <li>Source</li>
+        </ul>
       </div>
     );
   }
